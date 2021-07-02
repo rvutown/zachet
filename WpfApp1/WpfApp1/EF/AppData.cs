02fcee159cb78b92544174b61df98c0dd3d06a31
rvutown <61923334+rvutown@users.noreply.github.com>
2021-07-02 09:49:33 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/EF/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.EF
+{
+    class AppData
+    {
+        public static Sales4Entities Context = new Sales4Entities();
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp1.EF.AppData;
+
+namespace WpfApp1
+{
+    
+    public partial class MainWindow : Window
+    {
+        private static DirectoryInfo dir = new DirectoryInfo("../");
+        private static string fullPath = System.IO.Path.Combine(dir.FullName, "f.txt");
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lwMain.ItemsSource = Context.Customer.ToList();
+        }
+    }
+
+}
